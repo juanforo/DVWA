3d222e6e3e3d281ace0d36f54f0cd80d89ae09b2
Julián Forero <juanforo94@hotmail.com>
2018-05-15 04:18:17 +0000

    Forcing stop of containers

--- verify_docker.sh
+++ verify_docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+set -x
 APP_NAME='dvwa'
-if [ ! "$(docker ps -q -f name=$APP_NAME)" ]; then
-    docker-compose kill && docker-compose rm
+if [ "$(docker ps -q -f name=$APP_NAME)" ]; then
+    docker-compose kill && docker-compose rm -f
 fi
